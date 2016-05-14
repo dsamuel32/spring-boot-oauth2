d0d6550b7087be481ab0e67fe44f1d4776e0ffc3
dsamuel32 <diego.samuel.alves@gmail.com>
2016-05-14 13:44:27 -0300

    Configuração servidor grunt para cliente angular.

---
+++ oauth-server-view/Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg:grunt.file.readJSON('package.json'),
+
+        watch:{
+            options:{livereload:true},
+            files:['**/*'],
+            //files:['app/**','server/**'],
+            tasks:[]
+            
+        },
+        express:{
+            all:{
+                options:{
+                    server:'server.js',
+                    hostname:'localhost',
+                    port:3000,
+                    bases:['./oauth-server-view'],
+                    livereload:true
+                    
+                }
+            }
+        }        
+//        open: {
+//            all: {
+//                path: 'http://localhost:3000/public/app/index.html'
+//            }
+//        }
+        
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-express');
+    grunt.registerTask('server',['express','watch']);
+
+};
+
---
+++ oauth-server-view/public/app/app.js
@@ -0,0 +1,4 @@
+"use strict";
+angular.module('app', [ ]);
+//app.config([, function( ) {}]);
+//https://www.codetutorial.io/livereload-with-grunt/
